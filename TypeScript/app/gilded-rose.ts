1b6e9731519674b7a6d4d15cb1951d305ddfe9af
Daniel Leib <leib.mobil@gmail.com>
2018-05-17 00:49:02 +0200

    add basic tests

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -43,8 +43,6 @@ export class GildedRose {
                     } else {
                         item.quality = item.quality - item.quality
                     }
-                } else if (item.quality < 50) {
-                    item.quality = item.quality + 1
                 }
             }
         });
--- TypeScript/test/guilded-rose.spec.ts
+++ TypeScript/test/guilded-rose.spec.ts
@@ -1,12 +1,90 @@
 import { expect } from 'chai';
-import { Item, GildedRose } from '../app/gilded-rose';
+import { Item, GildedRose, Names } from '../app/gilded-rose';
+
+class Builder {
+    private gildedRose = new GildedRose();
+
+    add(name: string, sellIn: number, quality: number): this {
+        this.items.push(new Item(name, sellIn, quality));
+        return this;
+    }
+
+    update(): this {
+        this.gildedRose.updateQuality();
+        return this;
+    }
+
+    get items(): Item[] {
+        return this.gildedRose.items;
+    }
+
+    get first(): Item {
+        return this.items[0];
+    }
+}
 
 describe('Gilded Rose', () => {
-
-    it('should foo', function() {
-        const gildedRose = new GildedRose([ new Item('foo', 0, 0) ]);
-        gildedRose.updateQuality();
-        expect(gildedRose.items[0].name).to.equal('foo');
+    let builder: Builder;
+    beforeEach(() => {
+        builder = new Builder();
     });
 
+    it('should not change item without update', () => {
+        builder.add('foo', 0, 0);
+
+        expect(builder.first).to.eql({
+            name: 'foo',
+            sellIn: 0,
+            quality: 0
+        });
+    });
+
+    describe('Default Items', () => {
+        it('should decrease sellIn and quality', () => {
+            builder
+                .add('foo', 2, 2)
+                .update();
+
+            expect(builder.first).to.eql({
+                name: 'foo',
+                sellIn: 1,
+                quality: 1
+            })
+        });
+
+        it('should decrease sellIn below 0, but not quality', () => {
+            builder
+                .add('foo', 0, 0)
+                .update();
+
+            expect(builder.first.sellIn).to.eq(-1);
+            expect(builder.first.quality).to.eq(0);
+        });
+
+        it('should decrease quality by 2 if sellIn is 0', () => {
+            builder
+            .add('foo', 0, 10)
+            .update();
+
+            expect(builder.first.quality).to.eq(8);
+        });
+    });
+
+    describe('"Aged Brie" Items', () => {
+        it('should increase quality on update', () => {
+            builder
+            .add(Names.AGED_BRIE, 0, 0)
+            .update();
+
+            expect(builder.first.quality).to.eq(1);
+        });
+
+        it('max quality should be 50', () => {
+            builder
+            .add(Names.AGED_BRIE, 0, 50)
+            .update();
+
+            expect(builder.first.quality).to.eq(50);
+        });
+    });
 });
